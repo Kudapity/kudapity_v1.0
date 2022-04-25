bd406d9b28f81acfce3acafb954ec7a88700506e
Nazar <nazar.truhan789@gmail.com>
2022-04-25 10:08:23 +0300

    Cheeky

--- main.py
+++ main.py
@@ -19,14 +19,10 @@ class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   public_id = db.Column(db.String(50),unique=True)
   name = db.Column(db.String(50))
-  password = db.Column(db.String(80))
+  last_name = db.Column(db.String(50))
   admin = db.Column(db.Boolean)
 
-class Todo(db.Model):
-  id = db.Column(db.Integer,primary_key=True)
-  text = db.Column(db.String(50))
-  complete = db.Column(db.Boolean)
-  user_id = db.Column(db.Integer)
+
 
 def token_required(f):
   @wraps(f)
@@ -59,6 +55,7 @@ def get_all_users(current_user):
     user_data = {}
     user_data['public_id'] = user.public_id
     user_data['name'] = user.name
+    user_data['last_name'] = user.last_name
     user_data['password'] = user.password
     user_data['admin'] = user.admin
     output.append(user_data)
@@ -75,18 +72,16 @@ def get_one_user(current_user, public_id):
   user_data = {}
   user_data['public_id'] = user.public_id
   user_data['name'] = user.name
+  user_data['last_name'] = user.last_name
   user_data['password'] = user.password
   user_data['admin'] = user.admin
   return jsonify({'user': user_data})
 
 @app.route('/user', methods=['POST'])
-@token_required
-def create_user(current_user):
-  if not current_user.admin:
-    return jsonify({'message': 'Cannot perform that function!'})
+def create_user():
   data = request.get_json()
   hashed_password = generate_password_hash(data['password'], method='sha256')
-  new_user = User(public_id=str(uuid.uuid4()), name=data['name'], password=hashed_password, admin=False)
+  new_user = User(public_id=str(uuid.uuid4()), name=data['name'],last_name=data['last_name'], password=hashed_password, admin=False)
   db.session.add(new_user)
   db.session.commit()
   return jsonify({'message': 'New user created'})
@@ -132,65 +127,6 @@ def login():
     return jsonify({'token': token})
     
   return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
-
-@app.route('/todo', methods=['GET'])
-@token_required
-def get_all_todos(current_user):
-  todos = Todo.query.filter_by(user_id=current_user.id).all()
-  output = []
-  for todo in todos:
-    todo_data = {}
-    todo_data['id'] = todo.id
-    todo_data['text'] = todo.text
-    todo_data['complete'] = todo.complete
-    output.append(todo_data)
-  return jsonify({'todos': output})
-
-@app.route('/todo/<todo_id>', methods=['GET'])
-@token_required
-def get_one_todo(current_user, todo_id):
-  todo = Todo.query.filter_by(id=todo_id, user_id=current_user.id).first()
-  if not todo:
-    return jsonify({'message': 'No todo found!'})
-    
-  todo_data = {}
-  todo_data['id'] = todo.id
-  todo_data['text'] = todo.text
-  todo_data['complete'] = todo.complete
-    
-  return jsonify(todo_data)
-
-@app.route('/todo', methods=['POST'])
-@token_required
-def create_todo(current_user):
-  data = request.get_json()
-
-  new_todo = Todo(text=data['text'], complete=False, user_id=current_user.id)
-  db.session.add(new_todo)
-  db.session.commit()
-  return jsonify({'message': "Todo created!"})
-
-@app.route('/todo', methods=['PUT'])
-@token_required
-def complete_todo(current_user, todo_id):
-  todo = Todo.query.filter_by(id=todo_id, user_id=current_user.id).first()
-  if not todo:
-    return jsonify({'message': 'No todo found!'})
-
-  todo.complete = True
-  db.session.commit()
-  
-  return jsonify({'message': 'Todo item has been completed!'})
-
-@app.route('/todo/<todo_id>', methods=['DELETE'])
-@token_required
-def delete_todo(current_user, todo_id):
-  todo = Todo.query.filter_by(id=todo_id, user_id=current_user.id).first()
-  if not todo:
-    return jsonify({'message': 'No todo found!'})
-  db.session.delete(todo)
-  db.session.commit()
-  return jsonify({'message': 'Todo item deleted!'})
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0')
