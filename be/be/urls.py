ee8445ab5074397ef4374b03a26394632310e148
danyl <danylo3002@gmail.com>
2022-05-25 23:32:57 +0300

    added media root in urls

--- be/be/urls.py
+++ be/be/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('events/', include('events.urls')),
     path('', include('authentication.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
